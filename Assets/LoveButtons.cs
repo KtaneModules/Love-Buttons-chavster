cd93be25458a8fd4577742002fc2be79f46fb83d
chavster <aaronkenna2006@gmail.com>
2024-12-01 20:12:19 +0000

    Now with manual and button animations!

--- Assets/LoveButtons.cs
+++ Assets/LoveButtons.cs
@@ -19,6 +19,7 @@ public class LoveButtons : MonoBehaviour
 
     private string[] Keywords = { "Do you love me?", "Do you want to\nwatch a movie?", "Do you want to\nhang out?", "Do you need\nsome water?", "Do you want\nto kiss?", "Do you want\nto walk?" };
 
+    private Coroutine[] AnimCoroutines;
     private bool[] LoveCaps = new bool[4];
     private bool ModuleSolved;
 
@@ -26,15 +27,14 @@ public class LoveButtons : MonoBehaviour
     {
         _moduleId = ModuleIdCounter++;
         Module.OnActivate += Activate;
-        /*
-        foreach (KMSelectable object in keypad) {
-            object.OnInteract += delegate () { keypadPress(object); return false; };
-        }
-        */
-
-        //button.OnInteract += delegate () { buttonPress(); return false; };
+        AnimCoroutines = new Coroutine[Buttons.Length];
 
         DisplayText.text = Keywords[Rnd.Range(0, Keywords.Length)];
+        for (int i = 0; i < Buttons.Length; i++)
+        {
+            int x = i;
+            Buttons[i].OnInteract += delegate { ButtonPress(x); return false; };
+        }
     }
 
     void Activate()
@@ -44,8 +44,54 @@ public class LoveButtons : MonoBehaviour
 
     void Start()
     {
-        
+
     }
+    void ButtonPress(int pos)
+    {
+        Audio.PlayGameSoundAtTransform(KMSoundOverride.SoundEffect.ButtonPress, Buttons[pos].transform);
+        Buttons[pos].AddInteractionPunch();
+        if (AnimCoroutines[pos] != null)
+        {
+            StopCoroutine(AnimCoroutines[pos]);
+        }
+        AnimCoroutines[pos] = StartCoroutine(ButtonAnim(pos));
+
+    }
+    private IEnumerator ButtonAnim(int pos, float duration = 0.075f)
+    {
+        float init = 0.004f;
+        float depression = 0.002f;
+
+        Buttons[pos].transform.localPosition = new Vector3(Buttons[pos].transform.localPosition.x, init, Buttons[pos].transform.localPosition.z);
+
+        float timer = 0;
+        while (timer < duration) 
+        {
+            
+            Buttons[pos].transform.localPosition = new Vector3(Buttons[pos].transform.localPosition.x, Mathf.Lerp(init, init - depression, timer/duration), Buttons[pos].transform.localPosition.z);
+
+            yield return null; 
+            timer += Time.deltaTime;
+
+            
+        }
+        Buttons[pos].transform.localPosition = new Vector3(Buttons[pos].transform.localPosition.x, init - depression, Buttons[pos].transform.localPosition.z);
+
+
+
+        timer = 0;
+        while (timer < duration)
+        {
+            Buttons[pos].transform.localPosition = new Vector3(Buttons[pos].transform.localPosition.x, Mathf.Lerp(init - depression, init, timer / duration), Buttons[pos].transform.localPosition.z);
+
+            yield return null;
+            timer += Time.deltaTime;
+
+
+        }
+        Buttons[pos].transform.localPosition = new Vector3(Buttons[pos].transform.localPosition.x, init, Buttons[pos].transform.localPosition.z);
+    }
+
 
 #pragma warning disable 414
     private readonly string TwitchHelpMessage = @"Use !{0} to do something.";
