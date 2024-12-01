9542f6a094c8eb7ca695757173b153e41332202b
GhostSalt <mhanna756@c2ken.net>
2024-12-01 17:03:35 +0000

    -Some minor fixes, changes and chicanery

--- Assets/LoveButtons.cs
+++ Assets/LoveButtons.cs
@@ -2,76 +2,62 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text.RegularExpressions;
 using UnityEngine;
 using KModkit;
 using Rnd = UnityEngine.Random;
-using Math = ExMath;
-using UnityEngine.Collections;
 
-public class LoveButtons : MonoBehaviour {
- 
-   public KMBombInfo Bomb;
-   public KMAudio Audio;
-   public KMSelectable[] Button;
+public class LoveButtons : MonoBehaviour
+{
+    public KMBombModule Module;
+    public KMBombInfo Bomb;
+    public KMAudio Audio;
+    public KMSelectable[] Buttons;
     public TextMesh DisplayText;
 
-    string[] Keyword = { "Do you love me?", "Do you want to watch a movie?", "Do you want to hang out?", "Do you need some water?", "Do you want to kiss?", "Do you want to walk?" };
+    private int _moduleId;
+    private static int ModuleIdCounter = 1;
 
-    bool[] LoveCap = new bool[4];
-   static int ModuleIdCounter = 1;
-   int ModuleId;
-   private bool ModuleSolved;
+    private string[] Keywords = { "Do you love me?", "Do you want to\nwatch a movie?", "Do you want to\nhang out?", "Do you need\nsome water?", "Do you want\nto kiss?", "Do you want\nto walk?" };
 
-   void Awake () { //Avoid doing calculations in here regarding edgework. Just use this for setting up buttons for simplicity.
-      ModuleId = ModuleIdCounter++;
-      GetComponent<KMBombModule>().OnActivate += Activate;
-      /*
-      foreach (KMSelectable object in keypad) {
-          object.OnInteract += delegate () { keypadPress(object); return false; };
-      }
-      */
+    private bool[] LoveCaps = new bool[4];
+    private bool ModuleSolved;
 
-      //button.OnInteract += delegate () { buttonPress(); return false; };
-
-   }
-
-   void OnDestroy () { //Shit you need to do when the bomb ends
-      
-   }
-
-   void Activate () { //Shit that should happen when the bomb arrives (factory)/Lights turn on
-
-   }
-
-    void Start() { //Shit that you calculate, usually a majority if not all of the module
-        DisplayText.text = Keyword[Rnd.Range(0, Keyword.Length)];
-
-        
-   }
-
-   void Update () { //Shit that happens at any point after initialization
-
-   }
-
-   void Solve () {
-      
-   }
-
-    void Strike()
+    void Awake()
     {
-      
-   }
+        _moduleId = ModuleIdCounter++;
+        Module.OnActivate += Activate;
+        /*
+        foreach (KMSelectable object in keypad) {
+            object.OnInteract += delegate () { keypadPress(object); return false; };
+        }
+        */
+
+        //button.OnInteract += delegate () { buttonPress(); return false; };
+
+        DisplayText.text = Keywords[Rnd.Range(0, Keywords.Length)];
+    }
+
+    void Activate()
+    {
+
+    }
+
+    void Start()
+    {
+        
+    }
 
 #pragma warning disable 414
-   private readonly string TwitchHelpMessage = @"Use !{0} to do something.";
+    private readonly string TwitchHelpMessage = @"Use !{0} to do something.";
 #pragma warning restore 414
 
-   IEnumerator ProcessTwitchCommand (string Command) {
-      yield return null;
-   }
+    IEnumerator ProcessTwitchCommand(string Command)
+    {
+        yield return null;
+    }
 
-   IEnumerator TwitchHandleForcedSolve () {
-      yield return null;
-   }
+    IEnumerator TwitchHandleForcedSolve()
+    {
+        yield return null;
+    }
 }
